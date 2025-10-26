3958894dc1102d269dd0ca16a1e84764f7b94785
jordan711 <jordan.feng@hotmail.com>
2025-10-26 00:28:51 -0400

    Allow import of external list of sentences

--- process_words/src/main.rs
+++ process_words/src/main.rs
@@ -1,7 +1,10 @@
+mod sentences;
+
 #[derive(Debug)]
 enum WordProcessingError {
     EmptyWord,
     HasNumbers,
+    HasNonLetters
 }
 
 fn process_word(word: &str) -> Result<String, WordProcessingError> {
@@ -13,21 +16,31 @@ fn process_word(word: &str) -> Result<String, WordProcessingError> {
         return Err(WordProcessingError::HasNumbers);
     }
 
+    if word.chars().any(|c| !c.is_alphabetic()) {
+        return Err(WordProcessingError::HasNonLetters);
+    }
+
     return Ok(word.to_uppercase());
 }
 
 fn main() {
-    let sentence = "Hello, world!";
-    let words: Vec<&str> = sentence.split(" ").collect();
+    for sentence in sentences::SENTENCES {
+        println!("Processing each word in: {}", sentence);
+        let words: Vec<&str> = sentence.split(" ").collect();
 
-    let processed_results: Vec<Result<String, WordProcessingError>> =
-        words.iter().map(|&word| process_word(word)).collect();
+        let processed_results: Vec<Result<String, WordProcessingError>> =
+            words.iter().map(|&word| process_word(word)).collect();
 
-    for result in processed_results {
-        match result {
-            Ok(process_word) => println!("Processed {}", process_word),
-            Err(WordProcessingError::EmptyWord) => println!("Error: This is an empty word"),
-            Err(WordProcessingError::HasNumbers) => println!("Error: This word contains numbers")
+        for result in processed_results {
+            match result {
+                Ok(process_word) => println!("Processed {}", process_word),
+                Err(WordProcessingError::EmptyWord) => println!("Error: This is an empty word"),
+                Err(WordProcessingError::HasNumbers) => println!("Error: This word contains numbers"),
+                Err(WordProcessingError::HasNonLetters) => println!("Error: This word contains non-letters"),
+            }
         }
+
+        println!("----------------------\n\n")
     }
+
 }
---
+++ process_words/src/sentences.rs
@@ -0,0 +1,6 @@
+pub const SENTENCES: [&str;4] = [
+    "Hello, world!",
+    "Rust is a systems programming language",
+    "",
+    "@#$%$%@^@$%&%$@^$#^%#$"
+];
